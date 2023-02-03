5e17f30e0d0bf6c9ea4fbe40388c64fc06ccc31f
Brian <brian.salkas@gmail.com>
2023-02-02 20:58:54 -0600

    scrape urls now scrapes each url and stores --limit urls into pickle files

--- scrape_articles.py
+++ scrape_articles.py
@@ -1,15 +1,38 @@
 import requests
-#from scrape_each_article import scrape
+import sys
+import argparse
+from scrape_each_article import scrape
 from bs4 import BeautifulSoup
 import os
 import pickle
-path = '/home/brian/Documents/ai/science/pickle/'
+from_path = '/home/brian/Documents/ai/science/pickle/'
+to_path = '/home/brian/Documents/ai/science/science_articles/'
+all_articles = {}
 all_urls = set()
-for file in os.listdir(path):
-    print('FILE IS:',file,'-'*20)
-    with open(f'{path}{file}', 'rb') as f:
+# argparse stuff
+parser = argparse.ArgumentParser(description='scrape text form each url, store in pickle file')
+parser.add_argument('-l','--limit', default=float('inf'), help='set limit on number of urls to scrape')
+args = parser.parse_args()
+file_count = 1
+count= 0
+for file in os.listdir(from_path):
+    with open(f'{from_path}{file}', 'rb') as f:
         s = pickle.load(f)
         for url in s:
             all_urls.add(url)
-print(all_urls)
-print(len(all_urls))
+for url in all_urls:
+    text = scrape(url)
+    all_articles[url] = text
+    print(url)
+    count +=1 
+    if count >= int(args.limit):
+        print(f'writing file #{file_count}')
+        count = 0
+        with open(f'{to_path}/file_{file_count}', 'wb') as f:
+            pickle.dump(all_articles, f)
+        all_articles = {}
+
+with open(f'{to_path}/file_{file_count}', 'wb') as f:
+    pickle.dump(all_articles, f)
+        
+    
--- scrape_each_article.py
+++ scrape_each_article.py
@@ -4,15 +4,24 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 
 def scrape(url):
+    """rotate user-agent to avoid rate limit
+    check to make sure each page contains storytext div
+    scrape each p tag that is a direct child of the storytext"""
     sleep(1)
     ua = UserAgent()
+    print('HERE,',url)
     header = {'User-Agent': ua.random}
-    response = requests.get(url, headers=header)
+    try:
+        response = requests.get(url, headers=header,timeout=10)
+    except Exception:
+        print('took too long, no scrape')
     soup = BeautifulSoup(response.text, 'html.parser')
     storytext = soup.find('div',{'id': 'storytext'}) 
+    if not storytext:
+        return []
     text = []
     for each in storytext.findAll('p'):
-        text.append(each.text)
+        parent = each.parent
+        if parent.has_attr('id') and parent['id'] == 'storytext':
+            text.append(each.text)
     return " ".join(text)
-t = scrape('https://www.npr.org/2023/02/01/1153171109/tom-brady-retirement')
-print(t)
